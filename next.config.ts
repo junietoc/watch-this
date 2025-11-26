732ac5c0563d96627c573333a4a6d9a7b720b5d2
Juliana Nieto Cárdenas <jnietoca@purdue.edu>
2025-11-26 13:38:56 -0500

    actions

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   // Enable static export for GitHub Pages
   output: "export",
   
-  // GitHub Pages project path
-  basePath: "/watch-this",
-  
   // Disable image optimization (not supported in static export)
   images: {
     unoptimized: true,
